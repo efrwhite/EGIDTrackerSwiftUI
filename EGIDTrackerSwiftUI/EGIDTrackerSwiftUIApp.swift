801f9dcd44c4865609b9650f47c5248a46b25c76
Elizabeth White Baker <efrwhite@aol.com>
2026-02-28 02:21:19 -0500

    This commit adds the SwiftUI target to the UIKit target to have the SwiftUI app alongside the UIKit app in same project

---
+++ EGIDTrackerSwiftUI/EGIDTrackerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EGIDTrackerSwiftUIApp.swift
+//  EGIDTrackerSwiftUI
+//
+//  Created by Elizabeth Baker on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct EGIDTrackerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EGIDTrackerSwiftUITests/EGIDTrackerSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  EGIDTrackerSwiftUITests.swift
+//  EGIDTrackerSwiftUITests
+//
+//  Created by Elizabeth Baker on 2/28/26.
+//
+
+import Testing
+@testable import EGIDTrackerSwiftUI
+
+struct EGIDTrackerSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
